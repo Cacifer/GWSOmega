85078039fe9b18c75c5bb7815abaf817802da9c2
fredgrott <fred.grott@gmail.com>
2013-04-19 15:33:44 -0500

    first test run of javadoc generation,minor stuff to add project logo, the task to generate the uml image filesbut the ivy dependency managment works quite well

---
+++ docs/navtree_data.js
@@ -0,0 +1,19 @@
+var NAVTREE_DATA =
+[ [ "com.jakewharton.disklrucache", "reference/com/jakewharton/disklrucache/package-summary.html", [ [ "Classes", null, [ [ "DiskLruCache", "reference/com/jakewharton/disklrucache/DiskLruCache.html", null, null ], [ "DiskLruCache.Editor", "reference/com/jakewharton/disklrucache/DiskLruCache.Editor.html", null, null ], [ "DiskLruCache.Snapshot", "reference/com/jakewharton/disklrucache/DiskLruCache.Snapshot.html", null, null ], [ "Util", "reference/com/jakewharton/disklrucache/Util.html", null, null ] ]
+, null ] ]
+, null ], [ "com.wuman.twolevellrucache", "reference/com/wuman/twolevellrucache/package-summary.html", [ [ "Interfaces", null, [ [ "TwoLevelLruCache.Converter", "reference/com/wuman/twolevellrucache/TwoLevelLruCache.Converter.html", null, null ] ]
+, null ], [ "Classes", null, [ [ "TwoLevelLruCache", "reference/com/wuman/twolevellrucache/TwoLevelLruCache.html", null, null ] ]
+, null ] ]
+, null ], [ "gws.grottworkshop.gwsomega", "reference/gws/grottworkshop/gwsomega/package-summary.html", [ [ "Classes", null, [ [ "BuildConfig", "reference/gws/grottworkshop/gwsomega/BuildConfig.html", null, null ], [ "GWSApplication", "reference/gws/grottworkshop/gwsomega/GWSApplication.html", null, null ], [ "R", "reference/gws/grottworkshop/gwsomega/R.html", null, null ], [ "R.attr", "reference/gws/grottworkshop/gwsomega/R.attr.html", null, null ], [ "R.drawable", "reference/gws/grottworkshop/gwsomega/R.drawable.html", null, null ], [ "R.string", "reference/gws/grottworkshop/gwsomega/R.string.html", null, null ], [ "R.style", "reference/gws/grottworkshop/gwsomega/R.style.html", null, null ] ]
+, null ] ]
+, null ], [ "gws.grottworkshop.gwsomega.cache", "reference/gws/grottworkshop/gwsomega/cache/package-summary.html", [ [ "Classes", null, [ [ "BitmapLruCache", "reference/gws/grottworkshop/gwsomega/cache/BitmapLruCache.html", null, null ] ]
+, null ], [ "Enums", null, [ [ "BitmapLruCache.RecyclePolicy", "reference/gws/grottworkshop/gwsomega/cache/BitmapLruCache.RecyclePolicy.html", null, null ] ]
+, null ] ]
+, null ], [ "gws.grottworkshop.gwsomega.graphics.drawable", "reference/gws/grottworkshop/gwsomega/graphics/drawable/package-summary.html", [ [ "Classes", null, [ [ "CacheableBitmapDrawable", "reference/gws/grottworkshop/gwsomega/graphics/drawable/CacheableBitmapDrawable.html", null, null ] ]
+, null ] ]
+, null ], [ "gws.grottworkshop.gwsomega.runnable", "reference/gws/grottworkshop/gwsomega/runnable/package-summary.html", [ [ "Classes", null, [ [ "WeakReferenceRunnable", "reference/gws/grottworkshop/gwsomega/runnable/WeakReferenceRunnable.html", null, null ] ]
+, null ] ]
+, null ] ]
+
+;
+
---
+++ docs/reference/lists.js
@@ -0,0 +1,26 @@
+var DATA = [
+      { id:0, label:"com.jakewharton.disklrucache", link:"reference/com/jakewharton/disklrucache/package-summary.html", type:"package" },
+      { id:1, label:"com.jakewharton.disklrucache.DiskLruCache", link:"reference/com/jakewharton/disklrucache/DiskLruCache.html", type:"class" },
+      { id:2, label:"com.jakewharton.disklrucache.DiskLruCache.Editor", link:"reference/com/jakewharton/disklrucache/DiskLruCache.Editor.html", type:"class" },
+      { id:3, label:"com.jakewharton.disklrucache.DiskLruCache.Snapshot", link:"reference/com/jakewharton/disklrucache/DiskLruCache.Snapshot.html", type:"class" },
+      { id:4, label:"com.jakewharton.disklrucache.Util", link:"reference/com/jakewharton/disklrucache/Util.html", type:"class" },
+      { id:5, label:"com.wuman.twolevellrucache", link:"reference/com/wuman/twolevellrucache/package-summary.html", type:"package" },
+      { id:6, label:"com.wuman.twolevellrucache.TwoLevelLruCache", link:"reference/com/wuman/twolevellrucache/TwoLevelLruCache.html", type:"class" },
+      { id:7, label:"com.wuman.twolevellrucache.TwoLevelLruCache.Converter", link:"reference/com/wuman/twolevellrucache/TwoLevelLruCache.Converter.html", type:"class" },
+      { id:8, label:"gws.grottworkshop.gwsomega", link:"reference/gws/grottworkshop/gwsomega/package-summary.html", type:"package" },
+      { id:9, label:"gws.grottworkshop.gwsomega.BuildConfig", link:"reference/gws/grottworkshop/gwsomega/BuildConfig.html", type:"class" },
+      { id:10, label:"gws.grottworkshop.gwsomega.GWSApplication", link:"reference/gws/grottworkshop/gwsomega/GWSApplication.html", type:"class" },
+      { id:11, label:"gws.grottworkshop.gwsomega.R", link:"reference/gws/grottworkshop/gwsomega/R.html", type:"class" },
+      { id:12, label:"gws.grottworkshop.gwsomega.R.attr", link:"reference/gws/grottworkshop/gwsomega/R.attr.html", type:"class" },
+      { id:13, label:"gws.grottworkshop.gwsomega.R.drawable", link:"reference/gws/grottworkshop/gwsomega/R.drawable.html", type:"class" },
+      { id:14, label:"gws.grottworkshop.gwsomega.R.string", link:"reference/gws/grottworkshop/gwsomega/R.string.html", type:"class" },
+      { id:15, label:"gws.grottworkshop.gwsomega.R.style", link:"reference/gws/grottworkshop/gwsomega/R.style.html", type:"class" },
+      { id:16, label:"gws.grottworkshop.gwsomega.cache", link:"reference/gws/grottworkshop/gwsomega/cache/package-summary.html", type:"package" },
+      { id:17, label:"gws.grottworkshop.gwsomega.cache.BitmapLruCache", link:"reference/gws/grottworkshop/gwsomega/cache/BitmapLruCache.html", type:"class" },
+      { id:18, label:"gws.grottworkshop.gwsomega.cache.BitmapLruCache.RecyclePolicy", link:"reference/gws/grottworkshop/gwsomega/cache/BitmapLruCache.RecyclePolicy.html", type:"class" },
+      { id:19, label:"gws.grottworkshop.gwsomega.graphics.drawable", link:"reference/gws/grottworkshop/gwsomega/graphics/drawable/package-summary.html", type:"package" },
+      { id:20, label:"gws.grottworkshop.gwsomega.graphics.drawable.CacheableBitmapDrawable", link:"reference/gws/grottworkshop/gwsomega/graphics/drawable/CacheableBitmapDrawable.html", type:"class" },
+      { id:21, label:"gws.grottworkshop.gwsomega.runnable", link:"reference/gws/grottworkshop/gwsomega/runnable/package-summary.html", type:"package" },
+      { id:22, label:"gws.grottworkshop.gwsomega.runnable.WeakReferenceRunnable", link:"reference/gws/grottworkshop/gwsomega/runnable/WeakReferenceRunnable.html", type:"class" }
+
+    ];
