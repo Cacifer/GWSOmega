ff5dee2540346e1f2232e3d6537be23e2561c295
fredgrott <fred.grott@gmail.com>
2013-04-29 06:05:04 -0500

    wrapper for JK's salvage

---
+++ src/gws/grottworkshop/gwsomega/view/PageAdapter.java
@@ -0,0 +1,17 @@
+package gws.grottworkshop.gwsomega.view;
+
+import com.jakewharton.salvage.RecyclingPagerAdapter;
+
+/**
+ * PageAdapter, wrapper for Jake Wharton's salvage 
+ * RecyclingPageAdapter. Every component that imports 
+ * viewpager from support library,etc or extends 
+ * viewpager needs to be rewritten to extend or 
+ * import this wrapper.
+ * 
+ * @author fredgrott
+ *
+ */
+public abstract class PageAdapter extends RecyclingPagerAdapter {
+
+}
